d21993ad68e1b00575fa09f9be8309f8dfa96f06
Scott Cooper <scttcper@users.noreply.github.com>
2022-06-14 08:26:22 -0700

    fix: upgrade dependencies, use vitest (#19)

--- test/compare.spec.ts
+++ test/compare.spec.ts
@@ -1,69 +1,57 @@
-import test from 'ava';
+import { expect, it } from 'vitest';
 
 import { parseMAC } from '../src/index.js';
 
-const same = [
+it.each([
   '0a:0b:0c:0d:0e:0f',
   'a:b:c:d:e:f',
   '0a:b:c:d:e:0f',
   '0a-0b-0c-0d-0e-0f',
   'a-b-c-d-e-f',
   'a-0b-c-d-0e-f',
-];
-for (const input of same) {
-  test(`should detect if addresses are the same ${input}`, t => {
-    const addr = parseMAC('0a0b0c0d0e0f');
+])('should detect if addresses are the same %s', input => {
+  const addr = parseMAC('0a0b0c0d0e0f');
 
-    const other = parseMAC(input);
-    t.is(addr.compare(other), 0);
-  });
-}
+  const other = parseMAC(input);
+  expect(addr.compare(other)).toBe(0);
+});
 
-const different = [
+it.each([
   '0a:0b:0c:0d:0e:0e',
   '1:2:3:4:5:6',
   'a:b:c:d0:e:f',
   '0a-9b-0c-0d-0e-0f',
   'a-b-c-d-e-1',
   'a-b0-c-d-0e-f',
-];
-for (const input of different) {
-  test(`should detect if addresses are different ${input}`, t => {
-    const addr = parseMAC('0a0b0c0d0e0f');
+])('should detect if addresses are different %s', input => {
+  const addr = parseMAC('0a0b0c0d0e0f');
 
-    const other = parseMAC(input);
-    t.not(addr.compare(other), 0);
-  });
-}
+  const other = parseMAC(input);
+  expect(addr.compare(other)).not.toBe(0);
+});
 
-const before = [
+it.each([
   '0a:0b:0c:0d:0e:0e',
   '1:2:3:4:5:6',
   'a:b:c:d0:e:f',
   '0a-9b-0c-0d-0e-0f',
   'a-b-c-d-e-1',
   'a-b0-c-d-0e-f',
-];
-for (const input of before) {
-  test(`should detect if address comes before compared addresses ${input}`, t => {
-    const addr = parseMAC('00000c0d0e0f');
-    const other = parseMAC(input);
-    t.is(addr.compare(other), -1);
-  });
-}
+])('should detect if address comes before compared addresses', input => {
+  const addr = parseMAC('00000c0d0e0f');
+  const other = parseMAC(input);
+  expect(addr.compare(other)).toBe(-1);
+});
 
-const after = [
+it.each([
   '0a:0b:0c:0d:0e:0e',
   '1:2:3:4:5:6',
   'a:b:c:d0:e:f',
   '0a-9b-0c-0d-0e-0f',
   'a-b-c-d-e-1',
   'a-b0-c-d-0e-f',
-];
-for (const input of after) {
-  test(`should detect if address comes after compared addresses ${input}`, t => {
-    const addr = parseMAC('1f002c0d0e0f');
-    const other = parseMAC(input);
-    t.is(addr.compare(other), 1);
-  });
-}
+])('should detect if address comes after compared addresses', input => {
+  const addr = parseMAC('1f002c0d0e0f');
+  const other = parseMAC(input);
+  expect(addr.compare(other)).toBe(1);
+});
